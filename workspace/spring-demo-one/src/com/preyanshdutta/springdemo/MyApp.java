a8dee8f3f708dab416c1dd1eba4e8cf689da7bd1
kingpreyansh <preyanshdutta@gmail.com>
2022-01-03 11:16:49 -0500

    init

---
+++ workspace/spring-demo-one/src/com/preyanshdutta/springdemo/MyApp.java
@@ -0,0 +1,13 @@
+package com.preyanshdutta.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		
+		// create the object
+		Coach theCoach = new BasketballCoach();
+		
+		// use the object
+		System.out.println(theCoach.getDailyWorkout());
+	}
+}
